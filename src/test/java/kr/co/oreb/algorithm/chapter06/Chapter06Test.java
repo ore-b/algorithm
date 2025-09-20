9b069b3372eed37b993616c64efce4e3188f3a57
ore-b <turtle.oreb@gmail.com>
2025-09-20 23:03:30 +0900

    student sort

--- src/test/java/kr/co/oreb/algorithm/chapter06/Chapter06Test.java
+++ src/test/java/kr/co/oreb/algorithm/chapter06/Chapter06Test.java
@@ -1,5 +1,7 @@
 package kr.co.oreb.algorithm.chapter06;
 
+import kr.co.oreb.algorithm.chapter06.problems.StudentSort;
+import kr.co.oreb.algorithm.chapter06.problems.StudentSort.Student;
 import org.junit.jupiter.api.Test;
 
 public class Chapter06Test {
@@ -34,6 +36,19 @@ public class Chapter06Test {
         int[] array = {7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2};
 
         countSort.execute(array);
+    }
+
+    @Test
+    void studentSort() {
+        StudentSort studentSort = new StudentSort();
+        Student[] students = {
+            new Student("김철수", 100),
+            new Student("홍길동", 70),
+            new Student("임꺽정", 60),
+            new Student("아무개", 90),
+            new Student("김호호", 80),
+        };
+        studentSort.execute(students);
     }
 
 
